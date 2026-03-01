4eff524551af352bef1f394a826b121d9daf130b
Mudassir-Uddin <mudassiruddin351@gmil.com>
2026-03-01 20:09:00 +0500

    feat(auth): setup Laravel Breeze authentication with Blade stack

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,47 +8,55 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboardindex');
-})->name('dashboard');
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/Sections', [App\Http\Controllers\SectionsController::class, 'index'])->name('Sections.index');
-Route::get('/SectionsInsert', [App\Http\Controllers\SectionsController::class, 'create'])->name('Sections.create');
-Route::post('/SectionsStore', [App\Http\Controllers\SectionsController::class, 'store'])->name('Sections.store');
-Route::get('/SectionsEdit/{id}', [App\Http\Controllers\SectionsController::class, 'edit'])->name('Sections.edit');
-Route::post('/SectionsUpdate/{id}', [App\Http\Controllers\SectionsController::class, 'update'])->name('Sections.update');
-Route::get('/SectionsDestroy/{id}', [App\Http\Controllers\SectionsController::class, 'destroy'])->name('Sections.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-Route::get('/Academic_years', [App\Http\Controllers\Academic_YearsController::class, 'index'])->name('Academic_years.index');
-Route::get('/Academic_yearsInsert', [App\Http\Controllers\Academic_YearsController::class, 'create'])->name('Academic_years.create');
-Route::post('/Academic_yearsStore', [App\Http\Controllers\Academic_YearsController::class, 'store'])->name('Academic_years.store');
-Route::get('/Academic_yearsEdit/{id}', [App\Http\Controllers\Academic_YearsController::class, 'edit'])->name('Academic_years.edit');
-Route::post('/Academic_yearsUpdate/{id}', [App\Http\Controllers\Academic_YearsController::class, 'update'])->name('Academic_years.update');
-Route::get('/Academic_yearsDestroy/{id}', [App\Http\Controllers\Academic_YearsController::class, 'destroy'])->name('Academic_years.destroy');
+    Route::get('/Sections', [App\Http\Controllers\SectionsController::class, 'index'])->name('Sections.index');
+    Route::get('/SectionsInsert', [App\Http\Controllers\SectionsController::class, 'create'])->name('Sections.create');
+    Route::post('/SectionsStore', [App\Http\Controllers\SectionsController::class, 'store'])->name('Sections.store');
+    Route::get('/SectionsEdit/{id}', [App\Http\Controllers\SectionsController::class, 'edit'])->name('Sections.edit');
+    Route::post('/SectionsUpdate/{id}', [App\Http\Controllers\SectionsController::class, 'update'])->name('Sections.update');
+    Route::get('/SectionsDestroy/{id}', [App\Http\Controllers\SectionsController::class, 'destroy'])->name('Sections.destroy');
 
-Route::get('/Classes', [App\Http\Controllers\ClassesController::class, 'index'])->name('Classes.index');
-Route::get('/ClassesInsert', [App\Http\Controllers\ClassesController::class, 'create'])->name('Classes.create');
-Route::post('/ClassesStore', [App\Http\Controllers\ClassesController::class, 'store'])->name('Classes.store');
-Route::get('/ClassesEdit/{id}', [App\Http\Controllers\ClassesController::class, 'edit'])->name('Classes.edit');
-Route::post('/ClassesUpdate/{id}', [App\Http\Controllers\ClassesController::class, 'update'])->name('Classes.update');
-Route::get('/ClassesDestroy/{id}', [App\Http\Controllers\ClassesController::class, 'destroy'])->name('Classes.destroy');
+    Route::get('/Academic_years', [App\Http\Controllers\Academic_YearsController::class, 'index'])->name('Academic_years.index');
+    Route::get('/Academic_yearsInsert', [App\Http\Controllers\Academic_YearsController::class, 'create'])->name('Academic_years.create');
+    Route::post('/Academic_yearsStore', [App\Http\Controllers\Academic_YearsController::class, 'store'])->name('Academic_years.store');
+    Route::get('/Academic_yearsEdit/{id}', [App\Http\Controllers\Academic_YearsController::class, 'edit'])->name('Academic_years.edit');
+    Route::post('/Academic_yearsUpdate/{id}', [App\Http\Controllers\Academic_YearsController::class, 'update'])->name('Academic_years.update');
+    Route::get('/Academic_yearsDestroy/{id}', [App\Http\Controllers\Academic_YearsController::class, 'destroy'])->name('Academic_years.destroy');
 
-Route::get('/Subjects', [App\Http\Controllers\SubjectsController::class, 'index'])->name('Subjects.index');
-Route::get('/SubjectsInsert', [App\Http\Controllers\SubjectsController::class, 'create'])->name('Subjects.create');
-Route::post('/SubjectsStore', [App\Http\Controllers\SubjectsController::class, 'store'])->name('Subjects.store');
-Route::get('/SubjectsEdit/{id}', [App\Http\Controllers\SubjectsController::class, 'edit'])->name('Subjects.edit');
-Route::post('/SubjectsUpdate/{id}', [App\Http\Controllers\SubjectsController::class, 'update'])->name('Subjects.update');
-Route::get('/SubjectsDestroy/{id}', [App\Http\Controllers\SubjectsController::class, 'destroy'])->name('Subjects.destroy');
+    Route::get('/Classes', [App\Http\Controllers\ClassesController::class, 'index'])->name('Classes.index');
+    Route::get('/ClassesInsert', [App\Http\Controllers\ClassesController::class, 'create'])->name('Classes.create');
+    Route::post('/ClassesStore', [App\Http\Controllers\ClassesController::class, 'store'])->name('Classes.store');
+    Route::get('/ClassesEdit/{id}', [App\Http\Controllers\ClassesController::class, 'edit'])->name('Classes.edit');
+    Route::post('/ClassesUpdate/{id}', [App\Http\Controllers\ClassesController::class, 'update'])->name('Classes.update');
+    Route::get('/ClassesDestroy/{id}', [App\Http\Controllers\ClassesController::class, 'destroy'])->name('Classes.destroy');
 
-Route::get('/ClassSubjects', [App\Http\Controllers\ClassSubjectsController::class, 'index'])->name('ClassSubjects.index');
-Route::get('/ClassSubjectsInsert', [App\Http\Controllers\ClassSubjectsController::class, 'create'])->name('ClassSubjects.create');
-Route::post('/ClassSubjectsStore', [App\Http\Controllers\ClassSubjectsController::class, 'store'])->name('ClassSubjects.store');
-Route::get('/ClassSubjectsEdit/{id}', [App\Http\Controllers\ClassSubjectsController::class, 'edit'])->name('ClassSubjects.edit');
-Route::post('/ClassSubjectsUpdate/{id}', [App\Http\Controllers\ClassSubjectsController::class, 'update'])->name('ClassSubjects.update');
-Route::get('/ClassSubjectsDestroy/{id}', [App\Http\Controllers\ClassSubjectsController::class, 'destroy'])->name('ClassSubjects.destroy');
+    Route::get('/Subjects', [App\Http\Controllers\SubjectsController::class, 'index'])->name('Subjects.index');
+    Route::get('/SubjectsInsert', [App\Http\Controllers\SubjectsController::class, 'create'])->name('Subjects.create');
+    Route::post('/SubjectsStore', [App\Http\Controllers\SubjectsController::class, 'store'])->name('Subjects.store');
+    Route::get('/SubjectsEdit/{id}', [App\Http\Controllers\SubjectsController::class, 'edit'])->name('Subjects.edit');
+    Route::post('/SubjectsUpdate/{id}', [App\Http\Controllers\SubjectsController::class, 'update'])->name('Subjects.update');
+    Route::get('/SubjectsDestroy/{id}', [App\Http\Controllers\SubjectsController::class, 'destroy'])->name('Subjects.destroy');
 
-Route::get('/Parents', [App\Http\Controllers\ParentsController::class, 'index'])->name('Parents.index');
-Route::get('/ParentsInsert', [App\Http\Controllers\ParentsController::class, 'create'])->name('Parents.create');
-Route::post('/ParentsStore', [App\Http\Controllers\ParentsController::class, 'store'])->name('Parents.store');
-Route::get('/ParentsEdit/{id}', [App\Http\Controllers\ParentsController::class, 'edit'])->name('Parents.edit');
-Route::post('/ParentsUpdate/{id}', [App\Http\Controllers\ParentsController::class, 'update'])->name('Parents.update');
-Route::get('/ParentsDestroy/{id}', [App\Http\Controllers\ParentsController::class, 'destroy'])->name('Parents.destroy');
+    Route::get('/ClassSubjects', [App\Http\Controllers\ClassSubjectsController::class, 'index'])->name('ClassSubjects.index');
+    Route::get('/ClassSubjectsInsert', [App\Http\Controllers\ClassSubjectsController::class, 'create'])->name('ClassSubjects.create');
+    Route::post('/ClassSubjectsStore', [App\Http\Controllers\ClassSubjectsController::class, 'store'])->name('ClassSubjects.store');
+    Route::get('/ClassSubjectsEdit/{id}', [App\Http\Controllers\ClassSubjectsController::class, 'edit'])->name('ClassSubjects.edit');
+    Route::post('/ClassSubjectsUpdate/{id}', [App\Http\Controllers\ClassSubjectsController::class, 'update'])->name('ClassSubjects.update');
+    Route::get('/ClassSubjectsDestroy/{id}', [App\Http\Controllers\ClassSubjectsController::class, 'destroy'])->name('ClassSubjects.destroy');
+
+    Route::get('/Parents', [App\Http\Controllers\ParentsController::class, 'index'])->name('Parents.index');
+    Route::get('/ParentsInsert', [App\Http\Controllers\ParentsController::class, 'create'])->name('Parents.create');
+    Route::post('/ParentsStore', [App\Http\Controllers\ParentsController::class, 'store'])->name('Parents.store');
+    Route::get('/ParentsEdit/{id}', [App\Http\Controllers\ParentsController::class, 'edit'])->name('Parents.edit');
+    Route::post('/ParentsUpdate/{id}', [App\Http\Controllers\ParentsController::class, 'update'])->name('Parents.update');
+    Route::get('/ParentsDestroy/{id}', [App\Http\Controllers\ParentsController::class, 'destroy'])->name('Parents.destroy');
+});
+
+require __DIR__ . '/auth.php';
